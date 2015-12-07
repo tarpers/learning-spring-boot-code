3b4a9f62ccdafdafdd0f3385d530c9b396cd80e6
simon.tarplin <simon.tarplin@gamesys.co.uk>
2015-12-07 13:12:21 +0000

    Defining entities and repositories

--- ch4/teams/src/main/java/learningspringboot/TeamsApplication.java
+++ ch4/teams/src/main/java/learningspringboot/TeamsApplication.java
@@ -1,12 +1,29 @@
 package learningspringboot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class TeamsApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(TeamsApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(TeamsApplication.class, args);
+    }
+
+    @Autowired
+    TeammateRepository teammateRepository;
+
+    @PostConstruct
+    void seeTheRoster() {
+        for (Teammate teammate : teammateRepository.findAll()) {
+            log.info(teammate.toString());
+        }
     }
 }
