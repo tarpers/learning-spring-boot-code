631c6d38e1a70385bdc41319f64bacf9145a13cc
simon.tarplin <simon.tarplin@gamesys.co.uk>
2015-12-09 09:13:52 +0000

    Getting started (Ch5)

---
+++ ch5/teams-security/src/main/java/learningspringboot/TeamsSecurityApplication.java
@@ -0,0 +1,12 @@
+package learningspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamsSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamsSecurityApplication.class, args);
+    }
+}
